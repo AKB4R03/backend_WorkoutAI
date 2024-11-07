18df06706e6de7c8f10d8779e96b6943d3a1987f
AKB4R03 <masakbar@gmail.com>
2024-11-07 11:49:48 +0700

    test mongo

--- src/db/config/index.ts
+++ src/db/config/index.ts
@@ -2,7 +2,7 @@ import { MongoClient } from "mongodb";
 
 // console.log(process.env.MONGODB_CONNECTION_STRING);
 
-const connectionDb = process.env.MONGODB_URI;
+const connectionDb = process.env.MONGODB_CONNECTION_STRING;
 if (!connectionDb) {
   throw new Error("MONGODB_CONNECTION_STRING is not defined");
 }
